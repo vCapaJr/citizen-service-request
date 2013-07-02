55bb8fe03bc529d48e2db49317ed75553de70c24
MikeTschudi <mtschudi@esri.com>
2013-07-02 07:08:09 -0700

    Minor format restoration for consistency

--- js/config.js
+++ js/config.js
@@ -1,20 +1,20 @@
 ﻿/*global dojo */
 /** @license
-| Version 10.2
-| Copyright 2012 Esri
-|
-| Licensed under the Apache License, Version 2.0 (the "License");
-| you may not use this file except in compliance with the License.
-| You may obtain a copy of the License at
-|
-|    http://www.apache.org/licenses/LICENSE-2.0
-|
-| Unless required by applicable law or agreed to in writing, software
-| distributed under the License is distributed on an "AS IS" BASIS,
-| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-| See the License for the specific language governing permissions and
-| limitations under the License.
-*/
+ | Version 10.2
+ | Copyright 2012 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
 dojo.provide("js.config");
 dojo.declare("js.config", null, {
 
@@ -79,7 +79,7 @@ dojo.declare("js.config", null, {
         MapURL: "http://tryitlive.arcgis.com/arcgis/rest/services/ImageryHybrid/MapServer"
     }],
     //This section allows you to add a feature or map service layer, it must be an layer, not a service
-    //use the following format {URL: ""},{URL: ""},... 	
+    //use the following format {URL: ""},{URL: ""},...
     ReferenceOverlays: [
     ],
 
